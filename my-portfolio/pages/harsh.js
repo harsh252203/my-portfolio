8659598b99f521702643f237c30312f34601665c
harsh25 <hs41061@gmail.com>
2022-10-07 00:25:56 +0530

    initial commit

---
+++ my-portfolio/pages/harsh.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const harsh = () => {
+  return (
+    <div>
+        <h1 className="sharma"> hello i am harsh sharma, fuck u vidit chawda</h1>
+    </div>
+  )
+}
+
+export default harsh
